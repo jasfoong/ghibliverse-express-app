e870bc58d2865ff2a989b04dad6f6e4963df640c
rellagant <barbara.cs@gmail.com>
2024-05-15 17:23:46 -0400

    added totoro image; added image paths in json file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express();
 const cors = require("cors");
+const fs = require('fs');
 
 app.use(cors());
 
 const characterRoute = require('./routes/characters');
 app.use('/characters', characterRoute);
 
+//Serve static files from the public folder
+app.use(express.static('public'));
 
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
