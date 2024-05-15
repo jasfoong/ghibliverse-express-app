a047a392420b80382dc23202de88731d883f6144
rellagant <barbara.cs@gmail.com>
2024-05-15 11:56:36 -0400

    added routing to server.js for functionality

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
+
+const characterRoute = require('./routes/characters');
+app.use('/characters', characterRoute);
 
 
 app.listen(8080, () => {
