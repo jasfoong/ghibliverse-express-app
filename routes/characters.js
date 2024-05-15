de6e031bdf635a2a2a925d111829be377f8209c0
rellagant <barbara.cs@gmail.com>
2024-05-15 11:32:33 -0400

    added HTTP GET routing for characters

---
+++ routes/characters.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+//function to read our data file
+function readCharacters() {
+  const characterData = fs.readFileSync("./data/characters.json");
+  const parsedData = JSON.parse(characterData);
+  return parsedData;
+}
+
+//GET endpoint for all characters
+router.get("/", (_req, res) => {
+  const characters = readCharacters();
+  console.log(characters);
+  res.json(characters);
+});
+
+//GET endpoint for individual character
+router.get("/:id", (req, res) => {
+  const character = readCharacters();
+  const singleCharacter = character.find(
+    (character) => character.id === req.params.id
+  );
+  res.json(singleCharacter);
+});
+
+//Export router for use in index.js
+module.exports = router; 
